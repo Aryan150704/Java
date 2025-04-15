8bb97c48ccf8dec9cb1dc49c1337afaeebb0ac26
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-04-15 22:29:05 +0530

    Create forEach.java

---
+++ BasicFundamentals/Control Statements/forEach.java
@@ -0,0 +1,11 @@
+// The for-each loop is mainly used to iterate over arrays or collections — automatically handles index movement.
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
